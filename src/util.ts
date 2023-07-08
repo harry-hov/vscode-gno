fd30f243ebdf6920e655b2124380d0524bf53210
Hariom Verma <hariom18599@gmail.com>
2023-07-08 20:54:09 +0530

    Add func `promptForMissingTool()`

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,16 @@
+import vscode = require('vscode');
+
+export const envPath = process.env['PATH'] || (process.platform === 'win32' ? process.env['Path'] : null);
 
 // getBinPath returns the path to the tool.
 // TODO: Implementation
 export function getBinPath(tool: string): string {
 	return tool;
 }
+
+export async function promptForMissingTool(toolName: string) {
+	vscode.window.showWarningMessage(
+		`${toolName} is not found. Please make sure it is installed and available in the PATH ${envPath}`
+	);
+}
+
